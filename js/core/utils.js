130b4dc319487818ad68cb3fe78f8e9f47e121ad
chrisostin <chrisostin@github.com>
2026-03-23 16:40:52 +0300

    fix: добавлен метод formatLargeNumber в utils.js

--- js/core/utils.js
+++ js/core/utils.js
@@ -78,6 +78,16 @@ const MORI_UTILS = {
         return '$' + price.toFixed(2);
     },
 
+    formatLargeNumber: function(num) {
+        if (num === null || num === undefined) return '0';
+        const abs = Math.abs(num);
+        if (abs >= 1e12) return (num / 1e12).toFixed(1) + 'T';
+        if (abs >= 1e9) return (num / 1e9).toFixed(1) + 'B';
+        if (abs >= 1e6) return (num / 1e6).toFixed(1) + 'M';
+        if (abs >= 1e3) return (num / 1e3).toFixed(1) + 'K';
+        return num.toString();
+    },
+
     formatMoney: function(amount, currency = 'USD', locale = 'ru-RU') {
         return new Intl.NumberFormat(locale, {
             style: 'currency',
