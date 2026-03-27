7f2edfa77168221fd32e172fe0610493fd0f00b6
chrisostin <chrisostin@github.com>
2026-03-27 09:02:33 +0300

    style: убран заголовок Портфель сверху

--- js/core/router.js
+++ js/core/router.js
@@ -23,13 +23,13 @@ const MORI_ROUTER = {
         },
 
         // Портфель
-     //   portfolio: {
+        portfolio: {
        //     title: 'Портфель',
-         //   module: 'MORI_PORTFOLIO',
-       //     guard: 'user',
-       //     icon: '📊',
-         //   id: 'portfolio'
-     //   },
+            module: 'MORI_PORTFOLIO',
+            guard: 'user',
+           icon: '📊',
+            id: 'portfolio'
+        },
 
         // MORI AI
         'ai-chat': {
