3d386f9e616fba63ade9b172051edd1f4dfe9551
chrisostin <chrisostin@github.com>
2026-04-09 00:42:31 +0300

    fix: force set access_level from MORI_APP in profile

--- js/modules/profile/index.js
+++ js/modules/profile/index.js
@@ -39,11 +39,11 @@ const MORI_PROFILE = {
     loadUserData: function() {
         if (window.MORI_USER && MORI_USER.current) {
             this.state.user = MORI_USER.current;
-            this.state.user.access_level = MORI_APP?.accessLevel || this.state.user.access_level || 'user';
             this.state.level = this.state.user.level || 1;
             this.state.exp = this.state.user.experience || 0;
             const progress = MORI_USER.getLevelProgress();
             this.state.nextLevelExp = progress.required || 500;
+            this.state.user.access_level = MORI_APP?.accessLevel || 'user';
         } else {
             this.state.user = {
                 id: 'user_' + Date.now(),
